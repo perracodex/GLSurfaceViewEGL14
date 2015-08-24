9be0f74895dcac2f244c591b6d033bc630f9d798
perracolabs <perracolabs@gmail.com>
2015-08-24 16:15:28 +0200

    Comments cleanup

--- com/perracolabs/egl14/GLSurfaceViewEGL14.java
+++ com/perracolabs/egl14/GLSurfaceViewEGL14.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
  * under the License.
@@ -30,7 +30,7 @@ import android.view.SurfaceView;
 
 /**
  * GLSurfaceView using EGl14 instead of EGL10
- * 
+ *
  * @author Perraco Labs (August-2015)
  * @repository https://github.com/perracolabs/GLSurfaceViewEGL14
  */
@@ -1711,7 +1711,7 @@ public class GLSurfaceViewEGL14 extends SurfaceView implements SurfaceHolder.Cal
 		/*
 		 * Tries once to acquire the right to use an EGL context. Does not block. Requires that we are already in the sGLThreadManager monitor when
 		 * this is called.
-		 * 
+		 *
 		 * @return true if the right to use an EGL context was acquired.
 		 */
 		public boolean tryAcquireEglContextLocked(final GLThread thread)
